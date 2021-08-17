5b4191d5b44aec09cff85b12de4d50c72fe0272d
DanilFD <happyaggresor@mail.ru>
2021-08-17 22:47:01 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Button, DatePicker, version} from "antd";
+
+function App() {
+    return (
+        <div>
+            <h1>antd version: {version}</h1>
+            <DatePicker/>
+            <Button type="primary" style={{marginLeft: 8}}>
+                Primary Button
+            </Button>
+        </div>
+    );
+}
+
+export default App;
