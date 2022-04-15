545630c8628802fad59a76134801a020665d427c
DanilFD <happyaggresor@mail.ru>
2022-04-15 21:14:06 +0300

    fix genres and platforms component

--- src/pages/games/game/genres/genres.tsx
+++ src/pages/games/game/genres/genres.tsx
@@ -5,15 +5,16 @@ import {usePushQuery} from "../../../../hooks/usePushQuery";
 
 type Props = {
     game: GameType
+    navigation:boolean
 }
 
-export const Genres = ({game}: Props) => {
+export const Genres = ({game, navigation = true}: Props) => {
     const pushQuery = usePushQuery()
     return (
         <div>
             Жанр: {game.genres.map((g, index) =>
             <React.Fragment key={g.slug}>
-                            <span onClick={() => pushQuery('genre', g.slug)} className={styles.genre}
+                            <span onClick={() => navigation && pushQuery('genre', g.slug)} className={styles.genre}
                                   key={g.id}>{g.title}</span>
                 {index !== game.genres.length - 1 && <span>, </span>}
             </React.Fragment>)}
--- src/pages/games/game/platforms/platforms.tsx
+++ src/pages/games/game/platforms/platforms.tsx
@@ -5,15 +5,17 @@ import styles from './platforms.module.scss'
 
 type Props = {
     game: GameType
+    navigation: boolean
 }
 
-export const Platforms = ({game}: Props) => {
+export const Platforms = ({game, navigation}: Props) => {
     const pushQuery = usePushQuery()
     return (
         <div>
             Платформа: {game.platforms.map((p, index) =>
             <React.Fragment key={p.slug}>
-                            <span onClick={() => pushQuery('platform', p.slug)} className={styles.platform}
+                            <span onClick={() => navigation && pushQuery('platform', p.slug)}
+                                  className={styles.platform}
                                   key={p.id}>{p.title}</span>
                 {index !== game.platforms.length - 1 && <span>, </span>}
             </React.Fragment>)}
