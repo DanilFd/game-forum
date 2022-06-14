f760c9cf49e288ea6a6dc3e67f3ab706d14a1b5b
DanilFD <happyaggresor@mail.ru>
2022-06-14 21:23:09 +0300

    changing data for deploying

--- src/http/index.ts
+++ src/http/index.ts
@@ -3,7 +3,7 @@ import authStore, {ACCESS_TOKEN, isDeprecated} from "../store/authStore";
 import jwtDecode from "jwt-decode";
 import {DecodedToken} from "../types/Auth/DecodedToken";
 
-const API_URL = 'https://react-frt.herokuapp.com/api/'
+const API_URL = 'https://react-bac.herokuapp.com/api/'
 
 export const api = axios.create({
     withCredentials: true,
